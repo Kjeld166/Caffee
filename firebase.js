05b08fae44e57c758cdb8fb2e112a87da000cd3d
Kjeld166 <kimiko166.13@gmail.com>
2025-09-01 09:52:55 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+﻿< !--firebase.js -->
+    <script type="module">
+  // Import Firebase SDKs
+        import {initializeApp} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+        import {getFirestore} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+        // 👉 Hier deine Firebase Config einsetzen
+        const firebaseConfig = {
+            apiKey: "DEIN_API_KEY",
+        authDomain: "DEIN_PROJEKT.firebaseapp.com",
+        projectId: "DEIN_PROJEKT_ID",
+        storageBucket: "DEIN_PROJEKT.appspot.com",
+        messagingSenderId: "DEINE_SENDER_ID",
+        appId: "DEINE_APP_ID"
+  };
+
+        // Init
+        const app = initializeApp(firebaseConfig);
+        export const db = getFirestore(app);
+    </script>
